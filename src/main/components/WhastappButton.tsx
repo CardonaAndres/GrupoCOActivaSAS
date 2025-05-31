662667cd1beef583bea6963609e53cd139e6bb11
Andrés Cardona <135064128+Totoy777@users.noreply.github.com>
2025-05-31 18:36:33 -0500

    fix: Añadir z-index al botón de WhatsApp para mejorar la superposición

--- src/main/components/WhastappButton.tsx
+++ src/main/components/WhastappButton.tsx
@@ -7,7 +7,7 @@ export const WhatsappButton = () => {
         target="_blank"
         rel="noopener noreferrer"
         aria-label="Hablar por WhatsApp"
-        className="fixed bottom-3 right-3 bg-green-500 hover:bg-green-600 text-white p-4 rounded-full shadow-lg flex items-center justify-center transition-transform hover:scale-105"
+        className="fixed bottom-3 right-3 z-50 bg-green-500 hover:bg-green-600 text-white p-4 rounded-full shadow-lg flex items-center justify-center transition-transform hover:scale-105"
       >
          <div className="relative left-1">
             <WhatsAppIcon />
